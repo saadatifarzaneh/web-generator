ef886cc2d66f13d7ceb5058e50428d28b19e74e8
Ebrahim Ansari <eebism@gmail.com>
2023-11-07 22:04:03 +0100

    second phase of implementation

---
+++ save-data.js
@@ -0,0 +1,48 @@
+import puppeteer from "puppeteer"
+import { promises as fs } from "fs"
+
+const output_dir = "outputs"
+const viewport = {
+    width: 1920, // width of the viewport in pixels
+    height: 1080, // height of the viewport in pixels
+};
+
+
+function findElements() {
+    let data = [];
+    document.querySelectorAll('.save').forEach(el => {
+        const rect = el.getBoundingClientRect();
+        data.push({
+            type: el.dataset.type,
+            x: rect.left,
+            y: rect.top,
+            width: rect.width,
+            height: rect.height
+        });
+    });
+    return data;
+}
+    
+export async function saveData() {
+    const browser = await puppeteer.launch({
+        headless: 'new' // Opt-in to the new headless mode
+        // ... include any other options you need
+    });
+    const page = await browser.newPage();
+
+    // Set the viewport size
+    await page.setViewport({ width: viewport.width, height: viewport.height });
+
+    for (let index = 1; index <= 1; index++) {
+        console.log(`To process page ${index}`)
+        await page.goto("http://localhost");
+
+        const screenshotPath = `${output_dir}/page_${index}.png`;
+        await page.screenshot({ path: screenshotPath });
+        const data = await page.evaluate(findElements)
+
+        await fs.writeFile(`${output_dir}/page_${index}.elements.json`, JSON.stringify(data, null, 2));
+    }
+    console.log(`Process finished`)
+    await browser.close();
+}
---
+++ server.js
@@ -0,0 +1,75 @@
+
+import express from 'express'
+import {RecaptchaV2 as Recaptcha} from 'express-recaptcha'
+var options = { hl: 'en' , type: 'image'}
+var recaptcha = new Recaptcha('6LfuT-UoAAAAAGK7rlMWNXAFxxqqax2zZQcMfFJh', '6LfuT-UoAAAAAJLymzHzZUD1zJENyr-n3rzoBLvt', options)
+
+function selectFrom(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+export function startServer() {
+    const app = express()
+
+    app.use(express.static('static'));
+    //app.set('views', 'views');
+    app.set('view engine', 'vash');
+
+
+    app.get('/', recaptcha.middleware.render, async (req, res) => {
+        res.status(200).render('index', {
+            recaptcha: res.recaptcha,
+            content: [
+                { template: 'menu' },
+                { template: 'image' },
+                {
+                    template: 'sidebar', data: ['menu', [
+                        { template: 'form2' },
+                        { template: 'half', data: ['image', 'form'] }
+                    ]]
+                },
+                {
+                    template: 'sidebar', data: ['menu', [{ template: 'form2' }]]
+                },
+                { template: 'recaptcha', data: res.recaptcha },
+                { template: 'half', data: ['image', 'form2'] },
+                { template: 'half', data: ['image', 'image'] },
+                { template: 'half', data: ['form', 'form'] }
+            ]
+        });
+    });
+
+    app.get('/random.css', (req, res) => {
+        const font = selectFrom(['Barlow', 'Inter', 'Merriweather', "'Open Sans'"]);
+        const fontSize = Math.floor(Math.random() * 16) + 16;
+        const sideColor = selectFrom(['#d54', '#393', '#000']);
+
+        const randomColor = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+        const randomBackground = () => {
+            const backGroundType = Math.random();
+            if (backGroundType < .55)
+                return `background-image: url('https://picsum.photos/seed/${Math.random().toString(36).substring(7)}/1920/1080'); background-size: cover;`;
+            if (backGroundType < .85)
+                return `background-color: ${randomColor()};`;
+            return '';
+        }
+
+        res.status(200).send(`
+        body, input, select {
+            font-family: ${font};
+            font-size: ${fontSize}px;
+        }
+
+        body {
+            ${randomBackground()};
+        }
+
+        .side {
+            background: ${sideColor}
+        }
+    `);
+    })
+
+
+    return app.listen(80);
+}
