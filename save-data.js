513c44d93b82248aaa820b919e6ffbf1606b41f9
Ebrahim Ansari <eebism@gmail.com>
2024-02-29 19:40:44 +0100

    unifying all json outputs in one file

--- save-data.js
+++ save-data.js
@@ -8,30 +8,44 @@ const viewport = {
 };
 
 function findElements() {
-    let data = [];
+    let annotations = [];
     document.querySelectorAll('.save').forEach(el => {
         const rect = el.getBoundingClientRect();
-        data.push({
-            type: el.dataset.type,
-            x: rect.left,
-            y: rect.top,
-            width: rect.width,
-            height: rect.height
+        annotations.push({
+            type: "rectanglelabels",
+            value: {
+                id: 'XXXXXXXXXX',
+                x: rect.left,
+                y: rect.top,
+                width: rect.width,
+                height: rect.height,
+                type: el.dataset.type,
+                "rectanglelabels": [
+                    el.dataset.type
+                ],
+            }
         });
     });
 
     document.querySelectorAll('iframe[title="reCAPTCHA"]').forEach(el => {
         const rect = el.getBoundingClientRect();
-        data.push({
-            type: 'recaptcha',
-            x: rect.left,
-            y: rect.top,
-            width: rect.width,
-            height: rect.height
+        annotations.push({
+            type: "rectanglelabels",
+            value: {
+                id: 'XXXXXXXXXX',
+                x: rect.left,
+                y: rect.top,
+                width: rect.width,
+                height: rect.height,
+                type: 'recaptcha',
+                "rectanglelabels": [
+                    'recaptcha'
+                ],
+            }
         });
     });
     
-    return data;
+    return annotations;
 }
     
 export async function saveData(numPages) {
@@ -43,17 +57,30 @@ export async function saveData(numPages) {
 
     // Set the viewport size
     await page.setViewport({ width: viewport.width, height: viewport.height });
-
+    let shift_ids = 0
+    let final_json = [];
     for (let index = 1; index <= numPages; index++) {
         console.log(`Processing page ${index}/${numPages}`)
         await page.goto("http://localhost");
 
         const screenshotPath = `${output_dir}/page_${index}.png`;
         await page.screenshot({ path: screenshotPath, fullPage: false });
-        const data = await page.evaluate(findElements)
+        const page_annotations = await page.evaluate(findElements)
+        let page_id = index + shift_ids
+        final_json.push({
+            id: page_id,
+            annotations: [{
+                id: page_id,
+                result: page_annotations
+            }],
+            file_upload: `page_${index}.png`,
+            data: {image: screenshotPath},
+        });
 
-        await fs.writeFile(`${output_dir}/page_${index}.elements.json`, JSON.stringify(data, null, 2));
+        await fs.writeFile(`${output_dir}/page_${index}.elements.json`, JSON.stringify(page_annotations, null, 4));
     }
+    await fs.writeFile(`${output_dir}/all_elements.json`, JSON.stringify(final_json, null, 4));
+    
     console.log(`Process finished`)
     await browser.close();
 }
